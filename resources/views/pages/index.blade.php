65b8aea2c103148c2cb57059c5033bb37c04ad53
philiprosales <abayrosales@gmail.com>
2019-07-09 19:45:10 +0800

    first commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,7 @@
+<!doctype html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>This is the laravel application from the "Laravel From Scratch" Youtube Series</p>
+@endsection
