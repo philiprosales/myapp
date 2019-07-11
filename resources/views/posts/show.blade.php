cf62c0f4431073c1e213f72c194d6ad908b32c26
philiprosales <abayrosales@gmail.com>
2019-07-11 22:00:41 +0800

    commit to see if its working

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/posts" class="btn btn-default">Go Back</a>
+    <a href="/posts" class="btn btn-default">Back</a>
     <h1>{{$post -> title}}</h1>
     <div>
         {{$post->body}}
